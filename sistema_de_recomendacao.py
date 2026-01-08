43c2d656a2d49e77178a87e978cff698a9d60e37
80119050 <marcocardoso@correios.com.br>
2026-01-08 09:11:25 -0300

    Cap08 - 3C

--- sistema_de_recomendacao.py
+++ sistema_de_recomendacao.py
@@ -27,5 +27,20 @@ df_final_filmes = filmes [['movie_id', 'title', 'genres', 'overview', 'keywords'
 #print(df_final_filmes.isnull().sum())
 
 #Tem valores duplicados?
-print(df_final_filmes.duplicated().sum())
+#print(df_final_filmes.duplicated().sum())
+
+#processamento de textos com AST - abstract syntax tree. Vamos começar ajustando a coluna de gênero de filme. Nesta coluna, precisamos apenas dos nomes e não dos ids.
+# Função de conversão
+# Este loop itera sobre os elementos de uma estrutura de dados. 
+# Usamos ast.literal_eval(obj) para avaliar/converter a string obj em uma estrutura de dados Python.
+def converter(obj):
+  L = []
+  for i in ast.literal_eval(obj):
+    L.append(i['name'])
+  return L
+
+# Aplica-se, então, a função ao nosso dataset, nas colunas genres e keywords do filme
+df_final_filmes['genres'] = df_final_filmes['genres'].apply(converter)
+df_final_filmes['keywords'] = df_final_filmes['keywords'].apply(converter)
+print(df_final_filmes['keywords'].head())
 
